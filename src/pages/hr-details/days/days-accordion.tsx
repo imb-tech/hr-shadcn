961420dec88eeb893f7028e73549f925f0f594e7
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-06-20 17:56:29 +0500

    click

--- src/pages/hr-details/days/days-accordion.tsx
+++ src/pages/hr-details/days/days-accordion.tsx
@@ -12,6 +12,8 @@ import DaysTableHeader from "./days-header"
 import { getTimeDifference } from "../../office-detail/cols"
 import OneDaysAccordion from "./one-day-statistic"
 import { cn } from "@/lib/utils"
+import EmptyBox from "@/components/custom/empty-box"
+import AccordionSkeletion from "@/components/custom/accordion-skeletion"
 
 export const statusData: { [key: number | string]: string } = {
     0: "Kutilmoqda",
@@ -23,18 +25,19 @@ export default function DaysAccordion() {
     const navigate = useNavigate()
     const { id } = useParams({ from: "/_main/hr-view/$id" })
     const search: SearchParams = useSearch({ strict: false })
-    const { data: info } = useGet<HumanYear[]>(
-        `${USER_YEAR_TOTAL_MONTH_DAYS}/${id}`,
-        {
-            params: {
-                year: (search as any)?.year,
-                month: (search as any)?.month,
-            },
-            options: {
-                enabled: Boolean((search as any)?.month),
-            },
+    const {
+        data: info,
+        isLoading,
+        isSuccess,
+    } = useGet<HumanYear[]>(`${USER_YEAR_TOTAL_MONTH_DAYS}/${id}`, {
+        params: {
+            year: (search as any)?.year,
+            month: (search as any)?.month,
         },
-    )
+        options: {
+            enabled: Boolean((search as any)?.month),
+        },
+    })
 
     const clickAccordion = useCallback(
         (key: string) => {
@@ -54,73 +57,84 @@ export default function DaysAccordion() {
     return (
         <div>
             <DaysTableHeader />
-            {info?.map((item, index) => (
-                <Accordion
-                    type="single"
-                    collapsible
-                    className="w-full"
-                    value={search?.day ?? undefined}
-                    onValueChange={(val) => {
-                        clickAccordion(val)
-                    }}
-                >
-                    <AccordionItem
-                        value={String(item?.date?.slice(-2))}
-                        className={cn(
-                            index === info.length - 1 && "!border-none",
-                        )}
+            {isLoading && <AccordionSkeletion columnCount={6} rowCount={2} />}
+
+            {isSuccess && info?.length === 0 && (
+                <div className="bg-accent/20 rounded-b-md">
+                    <EmptyBox height="h-[30vh]" />
+                </div>
+            )}
+            {isSuccess &&
+                info?.map((item, index) => (
+                    <Accordion
+                        type="single"
+                        collapsible
+                        className="w-full"
+                        value={search?.day ?? undefined}
+                        onValueChange={(val) => {
+                            clickAccordion(val)
+                        }}
                     >
-                        <AccordionTrigger
+                        <AccordionItem
+                            value={String(item?.date?.slice(-2))}
                             className={cn(
-                                "px-3 dark:bg-accent/20 bg-stone-100 dark:data-[state=open]:bg-accent/20 data-[state=open]:bg-stone-100",
-                                index === info.length - 1 && "rounded-b-md",
-                                item?.date &&
-                                    item?.date?.slice(-2) == String(search?.day) &&
-                                    "rounded-b-md",
+                                index === info.length - 1 && "!border-none",
                             )}
                         >
-                            <div className="grid grid-cols-6 gap-10 text-start font-medium w-full">
-                                <p className="text-sm">
-                                    {item?.date ? item?.date?.slice(-2) : "-"}
-                                </p>
-                                <p className="text-sm">
-                                    {item?.work_shift_start?.slice(0, 5)} ~
-                                    {item?.work_shift_end?.slice(0, 5)}
-                                </p>
+                            <AccordionTrigger
+                                className={cn(
+                                    "px-3 dark:bg-accent/20 bg-stone-100 dark:data-[state=open]:bg-accent/20 data-[state=open]:bg-stone-100",
+                                    index === info.length - 1 && "rounded-b-md",
+                                    item?.date &&
+                                        item?.date?.slice(-2) ==
+                                            String(search?.day) &&
+                                        "rounded-b-md",
+                                )}
+                            >
+                                <div className="grid grid-cols-6 gap-10 text-start font-medium w-full">
+                                    <p className="text-sm">
+                                        {item?.date
+                                            ? item?.date?.slice(-2)
+                                            : "-"}
+                                    </p>
+                                    <p className="text-sm">
+                                        {item?.work_shift_start?.slice(0, 5)} ~
+                                        {item?.work_shift_end?.slice(0, 5)}
+                                    </p>
 
-                                <p className="text-sm">
-                                    {item?.attendance_time
-                                        ? item?.attendance_time?.slice(0, 5)
-                                        : "-"}
-                                </p>
+                                    <p className="text-sm">
+                                        {item?.attendance_time
+                                            ? item?.attendance_time?.slice(0, 5)
+                                            : "-"}
+                                    </p>
 
-                                <p className="text-sm">
-                                    {getTimeDifference(
-                                        item.work_shift_start,
-                                        item.attendance_time,
-                                    )}
-                                </p>
-                                <p className="text-sm">
-                                    {item?.left_time
-                                        ? item?.left_time?.slice(0, 5)
-                                        : "-"}
-                                </p>
-                                <p className="text-sm">
-                                    {item.left_time
-                                        ? getTimeDifference(
-                                              item.left_time,
-                                              item.work_shift_end,
-                                          )
-                                        : "-"}
-                                </p>
-                            </div>
-                        </AccordionTrigger>
-                        <AccordionContent className="flex  flex-col pl-6">
-                            <OneDaysAccordion />
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion>
-            ))}
+                                    <p className="text-sm">
+                                        {getTimeDifference(
+                                            item.work_shift_start,
+                                            item.attendance_time,
+                                        )}
+                                    </p>
+                                    <p className="text-sm">
+                                        {item?.left_time
+                                            ? item?.left_time?.slice(0, 5)
+                                            : "-"}
+                                    </p>
+                                    <p className="text-sm">
+                                        {item.left_time
+                                            ? getTimeDifference(
+                                                  item.left_time,
+                                                  item.work_shift_end,
+                                              )
+                                            : "-"}
+                                    </p>
+                                </div>
+                            </AccordionTrigger>
+                            <AccordionContent className="flex  flex-col pl-6">
+                                <OneDaysAccordion />
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                ))}
         </div>
     )
 }
--- src/pages/hr-details/months/month-accordion.tsx
+++ src/pages/hr-details/months/month-accordion.tsx
@@ -3,6 +3,7 @@ import { useGet } from "@/hooks/useGet"
 import { useNavigate, useParams, useSearch } from "@tanstack/react-router"
 import { useCallback } from "react"
 import DaysAccordion from "../days/days-accordion"
+import AccordionSkeletion from "@/components/custom/accordion-skeletion"
 import {
     Accordion,
     AccordionContent,
@@ -11,20 +12,22 @@ import {
 } from "@/components/ui/accordion"
 import MonthTableHeader from "./month-header"
 import { cn } from "@/lib/utils"
+import EmptyBox from "@/components/custom/empty-box"
 
 export default function MonthAccordion() {
     const navigate = useNavigate()
     const { id } = useParams({ from: "/_main/hr-view/$id" })
     const search = useSearch({ strict: false })
-    const { data: info } = useGet<HumanYear[]>(
-        `${USER_YEAR_TOTAL_MONTH}/${id}`,
-        {
-            params: { year: (search as any)?.year },
-            options: {
-                enabled: Boolean((search as any)?.year),
-            },
+    const {
+        data: info,
+        isLoading,
+        isSuccess,
+    } = useGet<HumanYear[]>(`${USER_YEAR_TOTAL_MONTH}/${id}`, {
+        params: { year: (search as any)?.year },
+        options: {
+            enabled: Boolean((search as any)?.year),
         },
-    )
+    })
 
     const clickAccordion = useCallback(
         (key: string) => {
@@ -44,48 +47,58 @@ export default function MonthAccordion() {
     return (
         <>
             <MonthTableHeader />
-            {info?.map((item, index) => (
-                <Accordion
-                    type="single"
-                    collapsible
-                    className="w-full"
-                    value={search?.month ?? undefined}
-                    onValueChange={(val) => {
-                        clickAccordion(val)
-                    }}
-                >
-                    <AccordionItem value={item?.month?.toString()}
-                     className={cn(
-                            index === info.length - 1 && "!border-none",
-                        )}>
-                        <AccordionTrigger
+            {isLoading && <AccordionSkeletion columnCount={4} rowCount={2} />}
+
+            {isSuccess && info?.length === 0 && (
+                <div className="dark:bg-secondary/40 bg-gray-200/80  rounded-b-md">
+                    <EmptyBox height="h-[30vh]" />
+                </div>
+            )}
+            {isSuccess &&
+                info?.map((item, index) => (
+                    <Accordion
+                        type="single"
+                        collapsible
+                        className="w-full"
+                        value={search?.month ?? undefined}
+                        onValueChange={(val) => {
+                            clickAccordion(val)
+                        }}
+                    >
+                        <AccordionItem
+                            value={item?.month?.toString()}
                             className={cn(
-                                "px-3 dark:bg-secondary/40 bg-gray-200/80 dark:data-[state=open]:bg-secondary/40 data-[state=open]:bg-gray-200/80",
-                                index === info.length - 1 && "rounded-b-md",
-                                item?.month &&
-                                    item?.month == String(search?.month) &&
-                                    "rounded-b-md",
+                                index === info.length - 1 && "!border-none",
                             )}
                         >
-                            <div className="grid grid-cols-4 gap-10 text-start font-medium w-full">
-                                <p className="text-sm">
-                                    {monthKeys[item.month as any]}
-                                </p>
-                                <p className="text-sm">
-                                    {item.late_count} marta
-                                </p>
-                                <p className="text-sm">
-                                    {item.late_duration?.slice(0, 5)}
-                                </p>
-                                <p className="text-sm">{item.fine} so'm</p>
-                            </div>
-                        </AccordionTrigger>
-                        <AccordionContent className="flex  flex-col pl-6">
-                            <DaysAccordion />
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion>
-            ))}
+                            <AccordionTrigger
+                                className={cn(
+                                    "px-3 dark:bg-secondary/40 bg-gray-200/80 dark:data-[state=open]:bg-secondary/40 data-[state=open]:bg-gray-200/80",
+                                    index === info.length - 1 && "rounded-b-md",
+                                    item?.month &&
+                                        item?.month == String(search?.month) &&
+                                        "rounded-b-md",
+                                )}
+                            >
+                                <div className="grid grid-cols-4 gap-10 text-start font-medium w-full">
+                                    <p className="text-sm">
+                                        {monthKeys[item.month as any]}
+                                    </p>
+                                    <p className="text-sm">
+                                        {item.late_count} marta
+                                    </p>
+                                    <p className="text-sm">
+                                        {item.late_duration?.slice(0, 5)}
+                                    </p>
+                                    <p className="text-sm">{item.fine} so'm</p>
+                                </div>
+                            </AccordionTrigger>
+                            <AccordionContent className="flex  flex-col pl-6">
+                                <DaysAccordion />
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                ))}
         </>
     )
 }
